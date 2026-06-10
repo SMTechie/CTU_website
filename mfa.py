947fb1f19913eb296311039f63a0927384c4a754
OpenSymphony <symphonythymagistrate71@gmail.com>
2026-06-10 11:03:28 +0200

    added the new web app for the portal in webapp folder and deleted the testing website. Edited the docker file to install python code for that specific container. also updated the yml code so that it seperates the two websites. changes were also made to the nginx so it could serve the login pages styles correctly. files were added from login, mfa and database branch without merging but with checkout

--- mfa.py
+++
@@ -1,5 +0,0 @@
-import pyotp
-
-def verify_mfa(secret, code):
-    totp = pyotp.TOTP(secret)
-    return totp.verify(code)
--- website/app.js
+++ website/app.js
@@ -16,6 +16,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
+// main route
+app.get("/websitename", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(3000, "0.0.0.0", () => {
     console.log("Webapp1 running on port 3000");
 });
