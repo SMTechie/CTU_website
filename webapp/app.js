04f3ac016830f9100327607ed6cb9025d422151a
OpenSymphony <symphonythymagistrate71@gmail.com>
2026-06-10 08:36:53 +0200

    Changed folder structure and duplicate app.js so that it would work with docker to show in the browser. Fixed certs to use port 443 and added the keys in. no changed were made to yaml also added college website in webapp just for testing

--- webapp/app.js
+++ webapp/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,4 +7,15 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+});*/
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => {
+    console.log("Webapp1 running on port 3000");
 });
