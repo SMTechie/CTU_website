07eecf3035ef195aabe2d5a353932f0d5d0dd48a
OpenSymphony <symphonythymagistrate71@gmail.com>
2026-06-11 16:33:41 +0200

    added new database code into database.sql. ALso edited app.js so that it could receive informations from forms

--- website/app.js
+++ website/app.js
@@ -1,25 +1,25 @@
-/*const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Portal Application Running");
-});
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});*/
-
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+// Lets express read JSON from frontend
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
+// Serve static frontend files
 app.use(express.static(path.join(__dirname, "public")));
 
 // main route
 app.get("/websitename", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.post("/websitename/contact-submit", (req, res) =>{
+    const {name, email, message} = req.body;
+
+    res.send({message: "Form received successfully!"});
+})
 
 app.listen(3000, "0.0.0.0", () => {
     console.log("Webapp1 running on port 3000");
